8fdb49c92c7984173cb461f361f7eec703c4a706
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2018-09-12 22:02:54 -0500

    Add model: Organizer

---
+++ migrations/20180913030133-create-organizer.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Organizers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      contactUrl: {
+        type: Sequelize.STRING
+      },
+      field: {
+        type: Sequelize.STRING
+      },
+      event_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Organizers');
+  }
+};
