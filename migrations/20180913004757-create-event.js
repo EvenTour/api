36c3d8815e6e23e00eb5759a7076069ddcef5b91
Enzo Lizama Paredes <lizama.enzo@gmail.com>
2018-09-23 23:50:57 -0500

    make it better

--- migrations/20180913004757-create-event.js
+++ migrations/20180913004757-create-event.js
@@ -23,6 +23,9 @@ module.exports = {
       duration: {
         type: Sequelize.INTEGER
       },
+      OrganizerId :{
+        type : Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
